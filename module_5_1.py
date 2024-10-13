bcb4b6311bc33d288a286288511308712b82d336
ws7465 <ws7465@ya.ru>
2024-10-13 22:02:14 +0300

    module_5_1.py 13.10.2024 22:02

--- module_5_1.py
+++ module_5_1.py
@@ -35,6 +35,7 @@ class House :
          self.number_of_floors = int(number_of_floors)
 #
       def go_to(self, new_floor) :
+         new_floor = int(new_floor)
          if 1 <= new_floor <= self.number_of_floors :
               for i in range(1, new_floor+1) :
                   print(i)
