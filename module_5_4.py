63cf0c4381a01ac47c87ccaf9cc6e8f7c5bd2319
ws7465 <ws7465@ya.ru>
2024-10-16 23:21:24 +0300

    module_5_4.py 16.10.2024 23:21

--- module_5_4.py
+++ module_5_4.py
@@ -25,100 +25,104 @@
 #
 # # # #
 #
-class House :
-      def __init__(self, name, number_of_floors) :
-         self.name = name # наименование строения
-         self.number_of_floors = int(number_of_floors) # кол-во его этажей
+class House : #
+    houses_history = [] # add 5_4
 #
-      def go_to(self, new_floor) :
-         new_floor = int(new_floor) # подъём на этаж строения
-         if 1 <= new_floor <= self.number_of_floors :
-              for i in range(1, new_floor+1) : # пройденные этажи
-                  print(i)
+
+
+    def __init__(self, name, number_of_floors) :
+        self.name = name # наименование строения
+        self.number_of_floors = int(number_of_floors) # кол-во его этажей
+#
+    def go_to(self, new_floor) :
+        new_floor = int(new_floor) # подъём на этаж строения
+        if 1 <= new_floor <= self.number_of_floors :
+            for i in range(1, new_floor+1) : # пройденные этажи
+                print(i)
+        else :
+            print("Такого этажа не существует")
+#
+    def __len__(self) :
+        return self.number_of_floors # высота здания в этажах
+#
+    def __str__(self) : # характеристика строения
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'  #
+#
+    def __eq__(self, other) : #  True,если __eq__(==).
+        if isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other.number_of_floors)
+            return akh1 == akh2
+        elif not isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other)
+            return akh1 == akh2
+#
+    def __add__(self, other) : # добавить (x + y)
+         if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+            return House(self.name, self.number_of_floors)
          else :
-              print("Такого этажа не существует")
+            self.number_of_floors += other
+            return House(self.name, self.number_of_floors)
 #
-      def __len__(self) :
-         return self.number_of_floors # высота здания в этажах
+    def __radd__(self, other) : # отражённое сложение x + y
+        return self.__add__(other)
 #
-      def __str__(self) : # характеристика строения
-         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'  #
+    def __iadd__(self, other):  # сложение с присваиванием x += y
+        self.number_of_floors = self.__add__(other)
+        return self.number_of_floors
 #
-      def __eq__(self, other) : #  True,если __eq__(==).
-          if isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other.number_of_floors)
-              return akh1 == akh2
-          elif not isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other)
-              return akh1 == akh2
+    def __lt__(self, other) : # True,если __lt__(<).
+        if isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other.number_of_floors)
+            return akh1 < akh2
+        elif not isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other)
+            return akh1 < akh2
 #
-      def __add__(self, other) : # добавить (x + y)
-           if isinstance(other, House):
-              self.number_of_floors += other.number_of_floors
-              return House(self.name, self.number_of_floors)
-           else :
-              self.number_of_floors += other
-              return House(self.name, self.number_of_floors)
+    def __le__(self, other) : # True, если , __le__(<=)
+        if isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other.number_of_floors)
+            return akh1 <= akh2
+        elif not isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other)
+            return akh1 <= akh2
 #
-      def __radd__(self, other) : # отражённое сложение x + y
-          return self.__add__(other)
-#
-      def __iadd__(self, other):  # сложение с присваиванием x += y
-          self.number_of_floors = self.__add__(other)
-          return self.number_of_floors
-#
-      def __lt__(self, other) : # True,если __lt__(<).
-          if isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other.number_of_floors)
-              return akh1 < akh2
-          elif not isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other)
-              return akh1 < akh2
-#
-      def __le__(self, other) : # True, если , __le__(<=)
-          if isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other.number_of_floors)
-              return akh1 <= akh2
-          elif not isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other)
-              return akh1 <= akh2
-#
-      def __gt__(self, other) : # True, если , __gt__(>)
-          if isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other.number_of_floors)
-              return akh1 > akh2
-          elif not isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other)
-              return akh1 > akh2
+    def __gt__(self, other) : # True, если , __gt__(>)
+        if isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other.number_of_floors)
+            return akh1 > akh2
+        elif not isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other)
+            return akh1 > akh2
 
 #
-      def __ge__(self, other) : # True, если , __ge__(>=)
-          if isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other.number_of_floors)
-              return akh1 >= akh2
-          elif not isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other)
-              return akh1 >= akh2
+    def __ge__(self, other) : # True, если , __ge__(>=)
+        if isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other.number_of_floors)
+            return akh1 >= akh2
+        elif not isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other)
+            return akh1 >= akh2
 #
-      def __ne__(self, other) : # True, если , __ne__(!=)
-          if isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other.number_of_floors)
-              return akh1 != akh2
-          elif not isinstance(other, House):
-              akh1 = int(self.number_of_floors)
-              akh2 = int(other)
-              return akh1 != akh2
+    def __ne__(self, other) : # True, если , __ne__(!=)
+        if isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other.number_of_floors)
+            return akh1 != akh2
+        elif not isinstance(other, House):
+            akh1 = int(self.number_of_floors)
+            akh2 = int(other)
+            return akh1 != akh2
 #
 # # # #
 #
